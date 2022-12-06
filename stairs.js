f3b6abfabf030599bc3e4057ffb70d987c4e1aeb
Olga <tulnikova2014@mail.ru>
2022-12-06 19:54:17 +0400

    timer

--- stairs.js
+++ stairs.js
@@ -10,92 +10,107 @@
 //     return acc;
 //   }, []);
 
-const makeCategoriesArray = (data) => {
-  let subsArray = [];
-  const subCategoriesArray = data.map(({ subCategories,parentCategory}) => (subCategories));
-  const parentCategorySubsArray = data.map(({parentCategory})=>{
-    if(parentCategory){
-     return parentCategory.subCategories;
-    }    
-  });
-  const filter = parentCategorySubsArray.filter((elem)=>!!elem);
-  return subsArray.concat(...subCategoriesArray, ...filter);
+// const makeCategoriesArray = (data) => {
+//   let subsArray = [];
+//   const subCategoriesArray = data.map(
+//     ({ subCategories, parentCategory }) => subCategories
+//   );
+//   const parentCategorySubsArray = data.map(({ parentCategory }) => {
+//     if (parentCategory) {
+//       return parentCategory.subCategories;
+//     }
+//   });
+//   const filter = parentCategorySubsArray.filter((elem) => !!elem);
+//   return subsArray.concat(...subCategoriesArray, ...filter);
+// };
+
+// const publishedAtArray = (data) =>
+//   data.reduce(
+//     (acc, { publishedAt, subCategories, parentCategory, products }) => {
+//       if (publishedAt) {
+//         acc = [...acc, publishedAt];
+//       }
+//       if (parentCategory) {
+//         acc = [...acc, parentCategory.publishedAt];
+//       }
+//       acc = [...acc, ...publishedAtArray(subCategories)];
+//       const filteredProductsPublishedAt = products.filter(
+//         (elem) => !!elem.publishedAt
+//       );
+//       const newArr = [...filteredProductsPublishedAt].map(
+//         (elem) => elem.publishedAt
+//       );
+//       acc = [...acc, ...newArr];
+//       return acc;
+//     },
+//     []
+//   );
+
+// const sortedDate = (data) => {
+//   const dateArr = publishedAtArray(data);
+//   const sorted = dateArr.sort((a, b) => Date.parse(a) - Date.parse(b));
+//   return sorted;
+// };
+
+// const takeAllTreePrice = (data) =>
+//   data.reduce((acc, { price }) => {
+//     if (price) {
+//       acc += price;
+//     }
+//     return acc;
+//   }, 0);
+
+// const priceSum = (data) =>
+//   data.reduce((acc, item) => {
+//     if (item.products.length) {
+//       acc += takeAllTreePrice(item.products);
+//     }
+//     if (item.subCategories.length) {
+//       acc += priceSum(item.subCategories);
+//     }
+//     return acc;
+//   }, 0);
+
+// const makeSubsObject = (data) => {
+//   const array = data.map(({ id, name }) => ({ id, name }));
+//   return array;
+// };
+// const makeObjectsArray = (data) => {
+//   let objectArray = [];
+//   return data.reduce((acc, item) => {
+//     const subCategories = makeSubsObject(item.subCategories);
+//     const object = {
+//       name: item.name,
+//       subCategories,
+//     };
+//     return (objectArray = [...objectArray, object]);
+//   }, []);
+// };
+
+// const stairsArray = fetch(
+//   "https://run.mocky.io/v3/c35ef044-e20d-4119-bfcc-421802d71a1e"
+// )
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//     console.log(makeCategoriesArray(data));
+//     console.log(sortedDate(data));
+//     console.log(priceSum(data));
+//     console.log(makeObjectsArray(data));
+//   });
+
+const timer = (inc) =>
+  new Promise((resolve, reject) => setTimeout(() => resolve(), 1000*inc));
+
+const logToStep = async () => {
+  let inc = 0;
+  while (inc < 10) {
+    await timer(inc);
+    inc += 1;
+    console.log(new Date())
+    console.log(inc);    
+  }
 };
-
-const publishedAtArray = (data) =>
-  data.reduce(
-    (acc, { publishedAt, subCategories, parentCategory, products }) => {
-      if (publishedAt) {
-        acc = [...acc, publishedAt];
-      }
-      if (parentCategory) {
-        acc = [...acc, parentCategory.publishedAt];
-      }
-      acc = [...acc, ...publishedAtArray(subCategories)];
-      const filteredProductsPublishedAt = products.filter(
-        (elem) => !!elem.publishedAt
-      );
-      const newArr = [...filteredProductsPublishedAt].map(
-        (elem) => elem.publishedAt
-      );
-      acc = [...acc, ...newArr];
-      return acc;
-    },
-    []
-  );
-
-const sortedDate = (data) => {
-  const dateArr = publishedAtArray(data);
-  const sorted = dateArr.sort((a, b) => Date.parse(a) - Date.parse(b));
-  return sorted;
-};
-
-const takeAllTreePrice = (data) =>
-  data.reduce((acc, { price }) => {
-    if (price) {
-      acc += price;
-    }
-    return acc;
-  }, 0);
-
-const priceSum = (data) =>
-  data.reduce((acc, item) => {
-    if (item.products.length) {
-      acc += takeAllTreePrice(item.products);
-    }
-    if (item.subCategories.length) {
-      acc += priceSum(item.subCategories);
-    }
-    return acc;
-  }, 0);
-
-const makeSubsObject = (data) => {
-  const array = data.map(({ id, name }) => ({ id, name }));
-  return array;
-};
-const makeObjectsArray = (data) => {
-  let objectArray = [];
-  return data.reduce((acc, item) => {
-    const subCategories = makeSubsObject(item.subCategories);
-    const object = {
-      name: item.name,
-      subCategories,
-    };
-   return objectArray = [...objectArray, object];
-  }, []);
-  // return objectArray;
-};
-
-const stairsArray = fetch(
-  "https://run.mocky.io/v3/c35ef044-e20d-4119-bfcc-421802d71a1e"
-)
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-    console.log(makeCategoriesArray(data));
-    console.log(sortedDate(data));
-    console.log(priceSum(data));
-    console.log(makeObjectsArray(data));
-  });
+logToStep();
